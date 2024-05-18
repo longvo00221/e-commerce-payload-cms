c1b7b55b9926ad5be870db3ca031f013c6056416
Long Vo <longvo@Longs-MacBook-Pro.local>
2024-05-18 16:50:31 +0700

    add filter provider into global provider

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -5,14 +5,16 @@ import React from 'react'
 import { AuthProvider } from '../_providers/Auth'
 import { CartProvider } from '../_providers/Cart'
 import { ThemeProvider } from './Theme'
-
+import {FilterProvider} from './Filter'
 export const Providers: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
   return (
     <ThemeProvider>
       <AuthProvider>
+        <FilterProvider>
         <CartProvider>{children}</CartProvider>
+        </FilterProvider>
       </AuthProvider>
     </ThemeProvider>
   )
